d7faa71e550e8e204a766b1e533007c8f173f83c
Anirudh Varma <anirudh@spotdraft.com>
2020-03-29 15:34:05 +0530

    Use before navigate to maintain source url for the current tab

--- src/background-script.ts
+++ src/background-script.ts
@@ -1,13 +1,5 @@
 import { Link, Store } from "./models";
-import {
-  findLatestLink,
-  findOrCreatePage,
-  findOrCreateSessionForPage,
-  getLinksForSession,
-  createSession,
-  findSession,
-  findSessionById
-} from "./store-helper";
+import { createSession, findLatestLink, findOrCreatePage, findSessionById, getLinksForSession } from "./store-helper";
 
 //TODO Remove params and fragments
 
@@ -28,6 +20,7 @@ export const LINK_LIST = "links";
 export const SESSION_LIST = "sessions";
 
 const tabSessionId: Record<number, number> = {};
+const sourceUrls: Record<number, string> = {};
 
 export const getStore = async (): Promise<Store> => {
   const store = await browser.storage.local.get();
@@ -42,41 +35,48 @@ async function onDomLoaded(details: NavigationDetail) {
   const links = store.links || [];
   const pages = store.pages || [];
   const existinSessionForTab = tabSessionId[details.tabId];
-  let lastPage = undefined;
   let session;
   if (!existinSessionForTab || (details.transitionType != "reload" && details.transitionType != "link")) {
     session = createSession();
     sessions.push(session);
     tabSessionId[details.tabId] = session.id;
-    console.log("Not reusing session", details.transitionType, existinSessionForTab);
   } else {
     session = findSessionById(sessions, existinSessionForTab);
-    const sessionLinks = getLinksForSession(links, existinSessionForTab);
-    lastPage = findLatestLink(sessionLinks);
-    console.log("reusing session", session.id);
   }
   const pageInfo = findOrCreatePage(details.url, currentTitle, pages);
   if (pageInfo.added) {
     pages.push(pageInfo.page);
   }
+  console.log("Source URL", sourceUrls[details.tabId], "target", details.url);
   const link: Link = {
     session: session.id,
-    source_url: lastPage?.target_url,
+    source_url: sourceUrls[details.tabId],
     target_url: details.url,
     timestamp: Date.now()
   };
+  sourceUrls[details.tabId] = details.url;
   links.push(link);
   browser.storage.local.set({ sessions, pages, links });
 }
 
 const filter: browser.webNavigation.EventUrlFilters = { url: [{ hostContains: ".wikipedia" }] };
 browser.webNavigation.onCommitted.addListener(onDomLoaded, filter);
-browser.webNavigation.onCreatedNavigationTarget.addListener(details => {
+browser.webNavigation.onCreatedNavigationTarget.addListener(async details => {
   // If the user opens in a new tab, sourceTabId maintains the session.
   if (details.sourceTabId != -1) {
     tabSessionId[details.tabId] = tabSessionId[details.sourceTabId];
+    const sourceTab = await browser.tabs.get(details.sourceTabId);
+    sourceUrls[details.tabId] = sourceTab.url;
+  } else {
+    sourceUrls[details.tabId] = details.url;
   }
 });
+browser.webNavigation.onBeforeNavigate.addListener(async details => {
+  const currentTab = await browser.tabs.query({ active: true, currentWindow: true });
+  if (!sourceUrls[details.tabId]) {
+    sourceUrls[details.tabId] = currentTab[0]?.url;
+  }
+}, filter);
 browser.storage.onChanged.addListener((changes, areaName) => {
-  // console.log(changes, areaName);
+  console.log(changes?.links?.newValue);
 });
